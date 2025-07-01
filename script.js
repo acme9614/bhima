ac11bb4a925a3ca6410dc09d421e713c02c8e3d0
Ajay <ajayjadhav85200@gmail.com>
2025-07-01 16:47:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,112 @@
+function openDrawer() {
+  document.getElementById("drawer").classList.remove("-translate-x-full");
+  document.getElementById("overlay").classList.remove("hidden");
+  document.getElementById("menuButton").classList.add("hidden");
+}
+
+function closeDrawer() {
+  document.getElementById("drawer").classList.add("-translate-x-full");
+  document.getElementById("overlay").classList.add("hidden");
+  document.getElementById("menuButton").classList.remove("hidden");
+}
+
+// drawers menu
+
+function navigateToScheme() {
+  Toaster.postMessage("navigateToScheme");
+}
+
+function navigateToBankDetails() {
+  Toaster.postMessage("navigateBankDetails");
+}
+
+function navigateToKyc() {
+  Toaster.postMessage("navigateKyc");
+}
+
+function navigateToRate() {
+  Toaster.postMessage("navigateRate");
+}
+
+function navigateSchemeRecords() {
+  Toaster.postMessage("navigateSchemeRecords");
+}
+
+function navigateToCustomerCard() {
+  Toaster.postMessage("navigateToCustomerCard");
+}
+
+function navigateTransactionHistory() {
+  Toaster.postMessage("navigateTransactionHistory");
+}
+
+function navigateToMyOrder() {
+  Toaster.postMessage("navigateToMyOrder");
+}
+
+function navigateToUserProfile() {
+  Toaster.postMessage("navigateToUserProfile");
+}
+
+function navigateToDigitalGold() {
+  Toaster.postMessage("navigateToDigitalGold");
+}
+
+function navigateToPanchang() {
+  Toaster.postMessage("navigateToPanchang");
+}
+
+function navigateToFeedback() {
+  Toaster.postMessage("navigateToFeedback");
+}
+
+function navigateToSetting() {
+  Toaster.postMessage("navigateToSetting");
+}
+
+function setUserName(username) {
+  // Find the element by ID and set the text content
+  document.getElementById("user-name-display").innerText =
+    "Welcome, " + username;
+}
+function navigateToHomePage() {
+  Toaster.postMessage("navigateToHomePage");
+}
+
+
+
+// mainSlider
+
+const swiper = new Swiper(".mainSlider", {
+    autoplay: {
+    delay: 3000, // Time between slides (in milliseconds)
+    disableOnInteraction: false, // Keep autoplay running after user interaction
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: false,
+});
+
+// scrollTopBtn
+
+ const scrollTopBtn = document.getElementById("scrollTopBtn");
+    const heroSection = document.getElementById("hero");
+
+    window.addEventListener("scroll", () => {
+      const heroBottom = heroSection.getBoundingClientRect().bottom;
+
+      if (heroBottom < 0) {
+        scrollTopBtn.classList.remove("opacity-0", "pointer-events-none");
+        scrollTopBtn.classList.add("opacity-100", "pointer-events-auto");
+      } else {
+        scrollTopBtn.classList.add("opacity-0", "pointer-events-none");
+        scrollTopBtn.classList.remove("opacity-100", "pointer-events-auto");
+      }
+    });
+
+    scrollTopBtn.addEventListener("click", () => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    });
